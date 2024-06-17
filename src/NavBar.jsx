03380c6ae22970e109d77d7011cc64dcae1bc632
sachinNishalka <nishalkasachin@gmail.com>
2024-06-17 11:42:29 +0530

    finished code

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,9 +1,15 @@
+import  { useState } from 'react';
 import logo from "./assets/logo.png";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faHouse, faUser, faHeart, faEnvelope, faGear, faRightFromBracket , faMinus } from '@fortawesome/free-solid-svg-icons'
+import { faHouse, faUser, faHeart, faEnvelope, faGear, faRightFromBracket, faMinus } from '@fortawesome/free-solid-svg-icons'
 
 const NavBar = () => {
-    const status = true
+  const [activeItem, setActiveItem] = useState('home');
+
+  const handleItemClick = (item) => {
+    setActiveItem(item);
+  };
+
   return (
     <div className="bg-white w-20 h-screen rounded-r-lg shadow-lg flex flex-col justify-between" style={{ height: 'calc(100vh - 2rem)', margin: '1rem 0' }}>
       <div className="pt-3">
@@ -11,26 +17,82 @@ const NavBar = () => {
         <h1 className="font-black font-bold text-center -mt-5">NATEAT</h1>
       </div>
       <div>
-            <ul className="text-center">
-                <li className="mt-5 py-2 hover:bg-purple-50   hover:shadow-none hover:ml-3 hover:rounded-l-md">
-                  <FontAwesomeIcon icon={faHouse} size="lg" style={{color: "#ffc43a"}}/>
-                  </li>
-                <div className={`${status ? 'mt-5 py-2 bg-purple-50  shadow-none ml-3 rounded-l-md' :''}`}>
-                        <li className={`${status ? ' bg-white  py-2 shadow-none ml-3 rounded-l-md ': ''}`} >
-                            <div className="flex flex-row justify-evenly">
-                                    <FontAwesomeIcon icon={faUser} size="lg" style={{color: "#ffc43a"}}/>
-                            {status ?<FontAwesomeIcon icon={faMinus} size="lg" style={{color: "#ffc43a"}}/>: ""}
-                            </div>
-                        </li>
-                </div>
-                <li className="mt-5 py-2 hover:bg-purple-50   hover:shadow-none hover:ml-3 hover:rounded-l-md"><FontAwesomeIcon icon={faHeart} size="lg" style={{color: "#ffc43a"}}/></li>
-                <li className="mt-5 py-2 hover:bg-purple-50   hover:shadow-none hover:ml-3 hover:rounded-l-md"><FontAwesomeIcon icon={faEnvelope} size="lg" style={{color: "#ffc43a"}}/></li>
-                <li className="mt-5 py-2 hover:bg-purple-50   hover:shadow-none hover:ml-3 hover:rounded-l-md"><FontAwesomeIcon icon={faGear} size="lg" style={{color: "#ffc43a"}}/></li>
-            </ul>
+        <ul className="text-center">
+
+          {/* <li className={`mt-5 py-2 ${activeItem === 'home' ? 'bg-purple-50 shadow-none ml-3 rounded-l-md' : ''}`} onClick={() => handleItemClick('home')}>
+            <FontAwesomeIcon icon={faHouse} size="lg" style={{color: "#ffc43a"}}/>
+          </li> */}
+
+
+            <div className={`${activeItem === 'home' ? 'mt-5 py-2 bg-purple-50  shadow-none ml-3 rounded-l-md' :'mt-5 py-2'}`}>
+                <li className={` ${activeItem === 'home' ? 'bg-white  py-2 shadow-none ml-3 rounded-l-md' : ''}`} onClick={() => handleItemClick('home')}>
+                    <div className="flex flex-row justify-evenly">
+                    <FontAwesomeIcon icon={faHouse} size="lg" style={{color: "#ffc43a"}}/>
+                    {activeItem === 'home' ? <FontAwesomeIcon icon={faMinus} size="lg" style={{color: "#ffc43a"}}/> : ""}
+                    </div>
+                </li>
+            </div>
+
+
+           <div className={`${activeItem === 'user' ? 'mt-5 py-2 bg-purple-50  shadow-none ml-3 rounded-l-md' :'mt-5 py-2'}`}>
+                <li className={` ${activeItem === 'user' ? 'bg-white  py-2 shadow-none ml-3 rounded-l-md' : ''}`} onClick={() => handleItemClick('user')}>
+                    <div className="flex flex-row justify-evenly">
+                    <FontAwesomeIcon icon={faUser} size="lg" style={{color: "#ffc43a"}}/>
+                    {activeItem === 'user' ? <FontAwesomeIcon icon={faMinus} size="lg" style={{color: "#ffc43a"}}/> : ""}
+                    </div>
+                </li>
+            </div> 
+
+          {/* <li className={`mt-5 py-2 ${activeItem === 'heart' ? 'bg-purple-50 shadow-none ml-3 rounded-l-md' : ''}`} onClick={() => handleItemClick('heart')}>
+            <FontAwesomeIcon icon={faHeart} size="lg" style={{color: "#ffc43a"}}/>
+          </li> */}
+
+           <div className={`${activeItem === 'heart' ? 'mt-5 py-2 bg-purple-50  shadow-none ml-3 rounded-l-md' :'mt-5 py-2'}`}>
+                <li className={` ${activeItem === 'heart' ? 'bg-white  py-2 shadow-none ml-3 rounded-l-md' : ''}`} onClick={() => handleItemClick('heart')}>
+                    <div className="flex flex-row justify-evenly">
+                    <FontAwesomeIcon icon={faHeart} size="lg" style={{color: "#ffc43a"}}/>
+                    {activeItem === 'heart' ? <FontAwesomeIcon icon={faMinus} size="lg" style={{color: "#ffc43a"}}/> : ""}
+                    </div>
+                </li>
+            </div> 
+
+
+          {/* <li className={`mt-5 py-2 ${activeItem === 'envelope' ? 'bg-purple-50 shadow-none ml-3 rounded-l-md' : ''}`} onClick={() => handleItemClick('envelope')}>
+            <FontAwesomeIcon icon={faEnvelope} size="lg" style={{color: "#ffc43a"}}/>
+          </li> */}
+
+            <div className={`${activeItem === 'envelope' ? 'mt-5 py-2 bg-purple-50  shadow-none ml-3 rounded-l-md' :'mt-5 py-2'}`}>
+                <li className={` ${activeItem === 'envelope' ? 'bg-white  py-2 shadow-none ml-3 rounded-l-md' : ''}`} onClick={() => handleItemClick('envelope')}>
+                    <div className="flex flex-row justify-evenly">
+                    <FontAwesomeIcon icon={faEnvelope} size="lg" style={{color: "#ffc43a"}}/>
+                    {activeItem === 'envelope' ? <FontAwesomeIcon icon={faMinus} size="lg" style={{color: "#ffc43a"}}/> : ""}
+                    </div>
+                </li>
+            </div> 
+
+
+          {/* <li className={`mt-5 py-2 ${activeItem === 'gear' ? 'bg-purple-50 shadow-none ml-3 rounded-l-md' : ''}`} onClick={() => handleItemClick('gear')}>
+            <FontAwesomeIcon icon={faGear} size="lg" style={{color: "#ffc43a"}}/>
+          </li> */}
+
+            <div className={`${activeItem === 'gear' ? 'mt-5 py-2 bg-purple-50  shadow-none ml-3 rounded-l-md' :'mt-5 py-2'}`}>
+                <li className={` ${activeItem === 'gear' ? 'bg-white  py-2 shadow-none ml-3 rounded-l-md' : ''}`} onClick={() => handleItemClick('gear')}>
+                    <div className="flex flex-row justify-evenly">
+                    <FontAwesomeIcon icon={faGear} size="lg" style={{color: "#ffc43a"}}/>
+                    {activeItem === 'gear' ? <FontAwesomeIcon icon={faMinus} size="lg" style={{color: "#ffc43a"}}/> : ""}
+                    </div>
+                </li>
+            </div> 
+
+
+
+        </ul>
       </div>
       <div className="pb-3">
         <ul className="text-center">
-          <li className="mt-5"><FontAwesomeIcon icon={faRightFromBracket} size="xl" style={{color: "#ffc43a"}}/></li>
+          <li className={`mt-5 ${activeItem === 'logout' ? 'bg-purple-50 shadow-none ml-3 rounded-l-md' : ''}`} onClick={() => handleItemClick('logout')}>
+            <FontAwesomeIcon icon={faRightFromBracket} size="xl" style={{color: "#ffc43a"}}/>
+          </li>
         </ul>
       </div>
     </div>
